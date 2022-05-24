4bc367a2078c354f256722b0784df2d7ca4fe294
Alexey Rubanik <rubanik.aleksey@gmail.com>
2022-05-24 22:00:44 +0300

    add calculus operations

---
+++ cmd/add.go
@@ -0,0 +1,71 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// get the flag value, its default value is false
+		fstatus, _ := cmd.Flags().GetBool("float")
+		if fstatus { // if status is true, call addFloat
+			addFloat(args)
+		} else {
+			addInt(args)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+	addCmd.Flags().BoolP("float", "f", false, "Add Floating Numbers")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func addInt(args []string) {
+	var result int
+	for _, number := range args {
+		if tmp, err := strconv.Atoi(number); err == nil {
+			result += tmp
+		}
+	}
+	fmt.Printf("Addition of numbers %s is %d", args, result)
+}
+
+func addFloat(args []string) {
+	var result float64
+	for _, v := range args {
+		// convert string to float64
+		ftemp, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+		result = result + ftemp
+	}
+	fmt.Printf("Sum of floating numbers %s is %f", args, result)
+}
--- main.go
+++ main.go
@@ -4,14 +4,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import (
-	"log"
-
-	"github.com/Rubanik-Alexei/AppCLI/cmd"
-)
+import "github.com/Rubanik-Alexei/AppCLI/cmd"
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		log.Fatal(err)
-	}
+	cmd.Execute()
 }
